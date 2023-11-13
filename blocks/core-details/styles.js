6cdceb7c5a73e0673a7d8f91c41c4a75bbd9fc4c
hanneslsm <HannesLeismann@gmail.com>
2023-11-13 23:09:38 +0100

    Add Details block chevron style

---
+++ blocks/core-details/styles.js
@@ -0,0 +1,11 @@
+/** Details
+ * @since ProLooks 4
+ * --------------------------------------------- */
+
+/**
+ * Register block styles
+ */
+wp.blocks.registerBlockStyle("core/details", {
+  name: "details-chevron",
+  label: "Chevron",
+});
