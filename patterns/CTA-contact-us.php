6d2039935933342e24c2816ce99a0286ecf48b73
hanneslsm <HannesLeismann@gmail.com>
2023-11-13 10:49:39 +0100

    fix color variables

--- patterns/CTA-contact-us.php
+++ patterns/CTA-contact-us.php
@@ -8,7 +8,7 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--x-large);margin-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"border":{"radius":"16px","width":"1px"},"background":{"backgroundImage":{"url":"http://prolooks.local/wp-content/uploads/2023/10/grid-bg-2-1920px.png","id":2533,"source":"file","title":"grid-bg-2-1920px"}}},"borderColor":"d-5","backgroundColor":"d-3","className":"is-style-default","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide is-style-default has-border-color has-d-5-border-color has-d-3-background-color has-background" style="border-width:1px;border-radius:16px;padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|colorc-4"}}}},"textColor":"c-3"} -->
+<div class="wp-block-group alignwide is-style-default has-border-color has-d-5-border-color has-d-3-background-color has-background" style="border-width:1px;border-radius:16px;padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|C-4"}}}},"textColor":"c-3"} -->
 <h2 class="wp-block-heading has-text-align-center has-c-3-color has-text-color has-link-color">Möchten Sie mehr erfahren oder haben Sie Fragen?</h2>
 <!-- /wp:heading -->
 
@@ -28,7 +28,7 @@
 <!-- /wp:image -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|colorc-4"}}}},"textColor":"c-3"} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|C-4"}}}},"textColor":"c-3"} -->
 <p class="has-text-align-center has-c-3-color has-text-color has-link-color"><strong>Telefon</strong></p>
 <!-- /wp:paragraph -->
 
@@ -46,7 +46,7 @@
 <!-- /wp:image -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|colorc-4"}}}},"textColor":"c-3"} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|C-4"}}}},"textColor":"c-3"} -->
 <p class="has-text-align-center has-c-3-color has-text-color has-link-color"><strong>E-Mail</strong></p>
 <!-- /wp:paragraph -->
 
