42d1ca9800f36ec91f38bb3349e8061ef2fbd11a
hanneslsm <HannesLeismann@gmail.com>
2023-11-13 22:48:31 +0100

    add picture frame image style

--- blocks/core-image/styles.js
+++ blocks/core-image/styles.js
@@ -9,3 +9,7 @@ wp.blocks.registerBlockStyle("core/image", {
   name: "image-hover-zoom",
   label: "Hover: Zoom",
 });
+wp.blocks.registerBlockStyle("core/image", {
+  name: "image-picture-frame",
+  label: "Picture Frame",
+});
