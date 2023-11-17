00e294e38e08fcb9bedf4c02d10084588e5c018f
hanneslsm <HannesLeismann@gmail.com>
2023-11-17 09:11:54 +0100

    Change Default Block to Primary

---
+++ blocks/core-button/variations.js
@@ -0,0 +1,13 @@
+/* Cover
+* @since ProLooks 3
+--------------------------------------------- */
+
+/**
+ * Defaults
+ */
+
+wp.blocks.registerBlockStyle("core/button", {
+  name: "primary",
+  label: "Primary",
+  isDefault: true,
+});
